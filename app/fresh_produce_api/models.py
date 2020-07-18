11588e4644f38da2702c9736a73c098659a8f7b3
NirvaanSingh <nirvaan.singh@vumatel.co.za>
2020-07-18 15:58:02 +0200

    Bug fixes, expected linting failure on tests

--- app/fresh_produce_api/models.py
+++ app/fresh_produce_api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length=200)
     price = models.FloatField()
-    grade = models.CharField(max_length = 5)
+    grade = models.CharField(max_length=5)
     min_quantity = models.IntegerField()
     is_in_stock = models.BooleanField()
