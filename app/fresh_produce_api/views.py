fbf862d4a3e3c6952a202b328ce227513142482e
NirvaanSingh <nirvaan.singh@vumatel.co.za>
2020-07-18 17:27:54 +0200

    bug fixes

--- app/fresh_produce_api/views.py
+++ app/fresh_produce_api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from fresh_produce_api import serializers, models
 
+
 # Create your views here.
 class ProductViewset(viewsets.ModelViewSet):
     serializer_class = serializers.ProductSerializer
+    
